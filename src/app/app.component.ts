d60506835f6ca6c0fccae95fb77b5b6db50ed373
wodahS21 <yaelelyolo@gmail.com>
2023-11-03 12:39:41 -0600

    Proyecto014 sobre pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto014';
+  nombre = 'Juan Carlos';
+  saldo = 1000.50;
+  dias = ['domingo','lunes','martes','miércoles','jueves','viernes','sábado'];
+  articulos = [{
+      codigo: 1,
+      descripcion: "papas",
+      precio: 12.33
+    },{
+      codigo: 2,
+      descripcion: "manzanas",
+      precio: 54
+    }];
+  fechaActual = new Date();
 }
